078851f958ae828255b59cf0af5afcd10117ba1f
wonho <wonho951@gmail.com>
2021-07-16 15:37:17 +0900

    2021.07.16 수업내용(el Jstl)

---
+++ WebContent/WEB-INF/views/03.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<h1>파라미터</h1>
+<!-- 	localhost:8088/elJstl/03?id=you&pw=1234&name=재석&num1=100&num2=200 	요청을 이렇게 하겠다는 뜻-->
+	
+	
+	<p>
+		
+	</p>
+</body>
+</html>
--- src/servlet/Servlet02.java
+++ src/servlet/Servlet02.java
@@ -15,7 +15,7 @@ public class Servlet02 extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		request.setAttribute("reqVal", "abcdefgh");
+		request.setAttribute("reqVal", "안녕하세요");
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/02.jsp");
 		rd.forward(request, response);
 	}
